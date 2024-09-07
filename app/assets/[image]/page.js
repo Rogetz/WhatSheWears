a26ef7f49dc658304f8aa05687a0d16edc5164a8
Rogetz <ronnyogetaz@gmail.com>
2024-09-07 17:56:33 +0300

    assets page changed

--- app/assets/[image]/page.js
+++ app/assets/[image]/page.js
@@ -4,5 +4,7 @@ export default AssetsPage({params}){
     const imagetext = `../${params.image}`
     console.log("the assets page found")
 
-    return <image src={imageText}/>
+    return (
+        <div>hello world</div>
+    )
 }
