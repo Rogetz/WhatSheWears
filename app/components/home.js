5ca64401f80cb7d6c18d78b2b9972d377659a71c
Rogetz <ronnyogetaz@gmail.com>
2024-09-07 13:54:49 +0300

    order button modified

--- app/components/home.js
+++ app/components/home.js
@@ -122,7 +122,7 @@ export function ActualHome(){
                         </div>
                         <div className="flex px-4 justify-between">
                             <p className="px-4 text-gray-400 font-medium">{product.category}</p>
-                            <button>order</button>
+                            <button className="rounded-lg px-4 py-2 bg-blue-600 text-white">order</button>
                         </div>
                     </Link>
 
